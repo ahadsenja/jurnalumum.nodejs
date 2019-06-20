2348d384765644b9aea4fa2db8dc1a8bc4cdceab
ahadsenja <hamdanilm96@gmail.com>
2019-06-21 04:46:08 +0800

    initial commit my project

---
+++ models/connection.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize('database', 'username', 'password', {
+    // sqlite! now!
+    dialect: 'sqlite',
+  
+    // the storage engine for sqlite
+    // - default ':memory:'
+    storage: 'migrations/jurnal.sqlite'
+  })
+
+  sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
