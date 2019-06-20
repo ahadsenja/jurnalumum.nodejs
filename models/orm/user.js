a3bd2250568d2fa8841d8ab56db5320dfe99437d
ahadsenja <hamdanilm96@gmail.com>
2019-06-21 04:57:44 +0800

    initial commit

--- models/orm/user.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize')
-const sqlCon = require('../connection')
-
-const User = sqlCon.define('user', {
-    username: Sequelize.STRING,
-    password: Sequelize.STRING
-})
-
-module.exports = User
