2eb5ebbda4427809fd00abcafc4424dddf4442b5
ahadsenja <hamdanilm96@gmail.com>
2019-06-21 05:42:56 +0800

    chore: update user model

--- orm/models/user.js
+++ orm/models/user.js
@@ -1,9 +1,12 @@
 const Sequelize = require('sequelize')
 const sqlCon = require('../connection')
+const Jurnal = require('./jurnal')
 
 const User = sqlCon.define('user', {
     username: Sequelize.STRING,
     password: Sequelize.STRING
 })
+
+User.sync({ force: true })
 
 module.exports = User
